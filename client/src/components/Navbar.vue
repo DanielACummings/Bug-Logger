248abb98da355ef224eeb521732c6de2942564ca
Daniel <danielac@protonmail.com>
2020-03-20 19:40:16 -0600

    turns navbar report bug form into modal

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -7,13 +7,62 @@
 				</router-link>
 			</div>
 			<div class="col-7">
-				<h3>Report A Bug</h3>
-				<form @submit.prevent="createBug">
-					<input required type="text" v-model="newBug.title" placeholder="Bug Title" />
-					<input required type="text" v-model="newBug.description" placeholder="Bug Description" />
-					<input required type="text" v-model="newBug.reportedBy" placeholder="Your Name" />
-					<button class="btn btn-success" type="submit">Report</button>
-				</form>
+				<div class="row">
+					<!-- Report bug modal -->
+					<div class="col-3">
+						<button
+							type="button"
+							class="btn btn-primary mt-4"
+							data-toggle="modal"
+							data-target="#reportBugModal"
+							data-whatever="@getbootstrap"
+						>Report Bug</button>
+
+						<div
+							class="modal fade"
+							id="reportBugModal"
+							tabindex="-1"
+							role="dialog"
+							aria-labelledby="reportBugModalLabel"
+							aria-hidden="true"
+						>
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="newPicModalLabel">Report Bug</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										<form @submit.prevent="createBug">
+											<div class="form-group">
+												<label for="title" class="col-form-label">Title:</label>
+												<input type="text" v-model="newBug.title" class="form-control" id="title" required />
+											</div>
+											<div class="form-group">
+												<label for="description" class="col-form-label">Description:</label>
+												<input type="text" v-model="newBug.description" class="form-control" id="description" />
+											</div>
+											<div class="form-group">
+												<label for="your-name" class="col-form-label">Your Name:</label>
+												<input
+													type="text"
+													v-model="newBug.reportedBy"
+													class="form-control"
+													id="your-name"
+													required
+												/>
+											</div>
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											<button type="submit" class="btn btn-primary">Submit</button>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
